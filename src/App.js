b2ecffcd3f77637ef3e1239d8d1037fbc656a933
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2022-05-10 10:03:36 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function HelloWorld() {
+  return (
+    <div style={{ backgroundColor: "blue", color: "white" }}>Hello World</div>
+  );
+}
+
+function Button() {
+  return <button onClick={() => alert("Anda berhasil")}>Click Saya</button>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HelloWorld />
+      <Button />
     </div>
   );
 }
